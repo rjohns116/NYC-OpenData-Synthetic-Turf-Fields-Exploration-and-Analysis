d767c681405d2fd32e770e56ae9a1e9a2cfc307d
rjohns116 <rlynjohnson6@gmail.com>
2026-01-26 00:12:29 -0500

    Add template prompts

---
+++ project.py
@@ -0,0 +1,5 @@
+#What will your software do? What features will it have? How will it be executed?
+#What new skills will you need to acquire? What topics will you need to research?
+#If working with one or two classmates, who will do what?
+#In the world of software, most everything takes longer to implement than you expect. And so it’s not uncommon to accomplish less in a fixed amount of time than you hope. 
+#What might you consider to be a good outcome for your project? A better outcome? The best outcome?
