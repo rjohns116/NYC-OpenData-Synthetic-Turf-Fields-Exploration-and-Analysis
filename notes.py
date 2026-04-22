42a74fe685516b9cb82d3a8a5c79dec29d6480d6
rjohns116 <rlynjohnson6@gmail.com>
2026-04-22 02:09:51 +0000

    Copied from VS code environment

---
+++ notes.py
@@ -0,0 +1,52 @@
+#this project is designed to clean and standardize the Synthetic Turf Fields dataset from NYC Open Data.
+#goal: transform real world data about turf fields in NYC to answer questions about usability/effectiveness
+
+#step by step
+#step 1: explore raw data, gaining an understanding of structure, data types, units, shape and general organization  
+
+#look for (go column by column):
+### shape(# of rows and columns)
+### categories that make sense
+### consistent column names
+### valid values and formatting for columns
+### valid data types for each column
+### missing data
+### duplicates
+### outliers
+### impossible values
+
+#step 2: create a log of errors to correct
+
+#step 3: write the code to restructure the dataset and log what you changed
+
+#step 4: Generate research questions to ask based on the data
+
+#Questions we can ask/answer based on this data
+
+### What boroughs have the most turf fields? Are they new and maintained or old?
+### Who maintains most of the fields and where? Are they active and accessible?
+### How many fields are located within a Jointly Operated Playground(JOP)? How many other fields are apart of a playground system?
+
+#step 5: write the code to analyze samples of the data in order to support the research questions
+
+#step 6: Decide what visualizations to generate
+
+#step 7: write the code to make charts and graphs
+
+
+
+#Conclusion/Future Research
+# #Why does this data matter?
+### indicates trends in quality and location of modern/technologically advanced city resources
+### can help explain geographical access barriers to publicly available recreation sites
+### can be used to predict vistor traffic based on quality
+### reflects patterns of city investment in public health especially across different regions
+### shows who has constructed and maintains the fields, having higher stake in what happens to them
+### shows if turf fields are an effective and easily accessible resource for supporting public health
+
+
+
+
+
+
+
--- project.py
+++ project.py
@@ -1,4 +1,167 @@
-#What will your software do? What features will it have? How will it be executed?
-#What new skills will you need to acquire? What topics will you need to research?
-#In the world of software, most everything takes longer to implement than you expect. And so it’s not uncommon to accomplish less in a fixed amount of time than you hope. 
-#What might you consider to be a good outcome for your project? A better outcome? The best outcome?
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+df = pd.read_csv("Synthetic_Turf_Fields_20260417.csv")
+
+#Step 1: Clean the data
+
+#Remove unnecessary columns
+df = df.drop(columns=["Contract_Type", 
+                      "Contract_Number", 
+                      "Shape", 
+                      "Maint_By_Spec", 
+                      "Construction_Entity_Spec"])
+
+#Rename columns to relevant descriptions in snake_case
+df = df.rename(columns={"SYSTEM":"Field_ID",
+                   "PARENTID":"Parent_ID",
+                   "DEPARTMENT":"Department",
+                   "DESCRIPTION":"Location_Description",
+                   "Maint_By":"Maintained_By",
+                   "SYNTURF_Location":"Cross_Streets",
+                   "RETIRED":"Retired",
+                   "FeatureStatus":"Status"
+                   })
+
+
+#Drop the parent_ID and "SYNTURF" from the Field_ID
+df["Field_ID"] = df["Field_ID"].str.extract(r"(0\d+$)")
+
+#remove the borough information from Parent_ID
+df["Parent_ID"] = df["Parent_ID"].str.extract(r"(\d+$)")
+
+#Change "Commission_Date" to datetime object
+df["Commission_Date"] = pd.to_datetime(df["Commission_Date"], format="%m/%d/%Y")
+
+#Replace all "Unknown" with NaN
+df.replace(["Unknown", "Not Applicable"], pd.NA, inplace =True)
+
+#Change "JOP" type to bool
+df["JOP"] = df["JOP"].astype(bool)
+
+#If the location contains "&" change it to "and"
+df["Cross_Streets"] = df["Cross_Streets"].replace(r"\s&\s", " and ", regex=True)
+
+#Combine "crumb rubber" and "rubber crumb" infill material types
+df["Infill_Material"] = df["Infill_Material"].replace("Crumb Rubber", "Rubber Crumb")
+
+
+#Expand borough names
+df["Borough"] = df["Borough"].replace({"B":"Brooklyn",
+                                       "M": "Manhattan",
+                                       "Q": "Queens",
+                                       "X": "Bronx",
+                                       "R": "Staten Island"})
+
+df.to_csv("cleaned_synthetic_turf_fields_data.csv", index=False)
+
+# Step 2: Start to answer analytical questions with the data
+
+#Q1: What boroughs have the most turf fields? Are they new and maintained or old?
+
+#Which borough has the most turf fields? 
+df.groupby("Borough").size() #Brooklyn contains the most with 104 fields
+b_fields = df[df["Borough"] == "Brooklyn"]
+
+#What percentage of those fields have 'infill' turf type?
+percent_infill = (b_fields["Turf_Type"] == "Infill").sum() / b_fields.shape[0] *100
+percent_infill = round(percent_infill, 1) #72.1% of fields are type "infill"
+
+#What does the distribution of infill materials look like?
+materials = b_fields.groupby("Infill_Material").size().drop("Non-Infill").sort_values(ascending=False)
+
+#How many fields are inactive?
+percent_inactive = (b_fields["Status"] != "Active").sum() / b_fields.shape[0] * 100
+percent_inactive = round(percent_inactive, 1) #20.2% of fields were archived, removed or inactive
+retired_count = b_fields["Retired"].sum() #0 fields are retired!
+
+#How many fields are less than 5 years old?
+newest_count = b_fields[b_fields["Commission_Date"] >= "2021-01-01"].shape[0] #27 fields are new(less than 5 years old)
+
+#How many fields are actively maintained by a company?
+percent_maintained = b_fields["Maintained_By"].notna().sum() / b_fields.shape[0] * 100
+percent_maintained = round(percent_maintained, 1) #91.3% of fields are maintained by a company!
+owners = b_fields.groupby("Maintained_By").size().sort_values(ascending=False) #Most of the fields are maintained by DPR.
+
+#Summary: NYC synthetic turf fields are concentrated in Brooklyn and show high quality and availability.
+
+#Q2: Who maintains most of the fields and where?
+
+#Who maintains the most fields and in what boroughs?
+df_quality = df[["Turf_Type", "Maintained_By", "Retired", "Borough", "Status" ]]
+
+owners_and_location = df_quality.groupby(["Maintained_By", "Borough"]).size() #DPR owns the most and they are the most evenly distributed across all five boroughs
+
+#How many fields in the city are are high quality 'infill' type fields?
+percent_infill = (df[df_quality["Turf_Type"] == "Infill"].shape[0] / df.shape[0] * 100)
+percent_infill = round(percent_infill, 1)
+
+#Are they active and not retired?
+dpr_fields = df[df_quality["Maintained_By"] == "DPR"]
+inactive = dpr_fields["Retired"].sum() + (dpr_fields["Status"] != "Active").sum()
+percent_inactive = inactive / dpr_fields.shape[0] * 100
+percent_inactive = round(percent_inactive,1) # 28% of DPR owned fields are archived, removed or inactive, but none are retired
+
+#Summary: DPR owns most of the fields in an even distribution across all boroughs - none are retired and 72% indicate "active" status
+
+#Q3: How many fields are within a Jointly Operated Playground(JOP)? How many other fields are apart of a playground system?
+
+#What percent of fields are located within a JOP?
+percent_jop = round(df["JOP"].sum() / df.shape[0] * 100, 1) #21.7% of fields are located within a JOP
+
+#How many fields are located in a play area, but not within a JOP?
+play_area = df[df["System_Type"] == "Play Area"]
+play_area = play_area[play_area["JOP"] == False]
+additional_fields = play_area.shape[0] #There are an additional 35 fields in the city that are located in a play area, just not specifically a JOP
+
+#Where are the fields concentrated?
+jop_fields = df[df["JOP"]]
+jop_distribution = jop_fields.groupby("Borough").size()
+play_area_fields = play_area
+play_area_distribuition = play_area.groupby("Borough").size() #Both JOP fields and additional play area fields are fairly evenly distributed across all 5 boroughs
+
+#What is the total percentage of fields located within or near a playground?
+total_play_fields = additional_fields + jop_fields.shape[0]
+percent_play_fields = round(total_play_fields / df.shape[0] * 100, 1)
+
+#Summary: 21.7% of fields are located within a JOP. Out of the fields that aren't, an additional 35 are located in a play area. Both are evenly distributed across all 5 boroughs. 
+
+#Step 3: Produce visuals to support findings
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
